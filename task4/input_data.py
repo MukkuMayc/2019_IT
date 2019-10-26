7d28572db886bf6344993b61ede2a203d39da399
MukkuMayc <st063775@student.spbu.ru>
2019-10-26 22:48:11 +0300

    Update input data

--- task4/input_data.py
+++ task4/input_data.py
@@ -1,8 +1,8 @@
 # Входные данные: X, shape=(N, M). N - число точек, M - число координат
 import numpy as np
 
-n = 1000
-m = 10
+n = 10
+m = 3
 scaler = 20
 
 X = np.trunc(np.random.rand(n, m) * scaler)
